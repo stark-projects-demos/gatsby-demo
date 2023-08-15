8b964c61848875f6bb9760266670a9d39526f1ae
Abhiraj Padhye <59491581+abcdefghiraj@users.noreply.github.com>
2023-08-15 17:22:43 +0200

    Use starter template

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,7 @@
-import "./src/styles/global.css"
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
+ */
+
+// You can delete this file if you're not using it
